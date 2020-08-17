8eca5102726f8c3f21c23d629bb188b866c8054b
Cameron Urnes <cameron@datanyze.com>
2020-08-17 00:10:35 -0700

    Removing default

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    EMBY_SERVER = os.environ.get('EMBY_SERVER') or 'http://127.0.0.1:8096'
+    EMBY_SERVER = os.environ.get('EMBY_SERVER')
     DEFAULT_ROOM = os.environ.get('DEFAULT_ROOM') or 'Bacon Bar'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
