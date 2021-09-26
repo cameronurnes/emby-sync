eca74f2ba63918bf0d863dfe1333ee1a24a6baed
Sidiox <sidiox@hotmail.com>
2021-09-26 17:14:44 +0200

    Reverted accidental INTERVAL change to 0.5 seconds

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
     EMBY_SERVER = os.environ.get('EMBY_SERVER')
     DEFAULT_ROOM = os.environ.get('DEFAULT_ROOM') or 'Bacon Bar'
-    INTERVAL = os.getenv("INTERVAL", "0.5")
+    INTERVAL = os.getenv("INTERVAL", "3.0")
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
